436e2533d756d604311d5af3336f7208faa9d906
cwu1367 <97273367+cwu1367@users.noreply.github.com>
2022-04-03 21:56:13 -0400

    Edited JS

---
+++ src/app.js
@@ -0,0 +1,21 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let windElement = document.querySelector("#wind");
+  let humidityElement = document.querySelector("humidity");
+  let feelsLikeElement = document.querySelector("feelsLike");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = response.data.main.humidity;
+  feelsLikeElement.innerHTML = response.data.main.feels_like;
+}
+
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
